649e4977457b7dacb4265514c86a5f492d2fb2bf
Beatriz Gomez <beaa2509@gmail.com>
2018-05-31 16:20:15 +0200

    basics of audio and speech procesing

---
+++ speech_recog/audio_signals.py
@@ -0,0 +1,12 @@
+# This python file, follows the basics of the chapter 12 of the artificial intelligence with python book
+# wth this file, the basic of the speech and audio procesing with python is explained
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('hola')
