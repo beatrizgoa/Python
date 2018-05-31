019752c66a9d620d86026a24799f3f7ded686e8b
Beatriz Gomez <beaa2509@gmail.com>
2018-05-31 16:59:34 +0200

    "start python file to work"

--- speech_recog/audio_signals.py
+++ speech_recog/audio_signals.py
@@ -1,12 +1,14 @@
 # This python file, follows the basics of the chapter 12 of the artificial intelligence with python book
 # wth this file, the basic of the speech and audio procesing with python is explained
 
-
-
-
-
+from scipy.io import wavfile
 
 
 if __name__ == '__main__':
 
+    #from scipy, wav file is imported
+    sampling_frequency, audio_signal = wavfile.read('random_sound.wav')
+
+
     print('hola')
+
