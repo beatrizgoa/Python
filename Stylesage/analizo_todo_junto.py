ea07a4fa90d90d04c9a0a88dccf2ec5c9dc227a7
gomezabeatriz <beaa2509@gmail.com>
2018-06-21 19:43:33 +0200

    Este fichero es para analizar los datos del training_modified_dummies y no juntarlo todo

---
+++ Stylesage/analizo_todo_junto.py
@@ -0,0 +1,16 @@
+# En este fichero de python, analizo la correlacion y esas cosas cuando no queda variables categoricas en el train y esta unido con lo que interesa de users y producto
+
+import pandas as pd
+from prueba import buscamos_correlacion
+
+def read_data(path = './', name='train.csv'):
+    data = pd.read_csv(path + name, delimiter=';', sep='delimiter')
+
+    return data
+
+
+
+if __name__ == '__main__':
+    train = read_data('./', 'trainining_modified_dummies.csv')
+
+    buscamos_correlacion(train)
