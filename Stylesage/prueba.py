0712e2600b11fb926846dead63e72d2e1948900d
gomezabeatriz <beaa2509@gmail.com>
2018-06-21 11:39:49 +0200

    Hemos creado un nuevo fichero csv para train, y otro para test, con los datos que ya tenian mas la marca del producto, fecha y paisdel usuario

--- Stylesage/prueba.py
+++ Stylesage/prueba.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 
 
 def read_data():
+
+    # Leemos los datos y mostramos la cabecera para examinarlos
+
     path = './Exercise/'
 
     train_data = pd.read_csv(path+"train.csv",delimiter=';', sep='delimiter')
@@ -29,17 +32,23 @@ def read_data():
 
 def check_nulls(train_data, test_data, users_data, products_data):
 
+    # Con esta funcion chekeamos si es nulo algún valor
+
     # Vamos a ver los null que hay
     train_null = train_data.isnull().sum()
     test_null = test_data.isnull().sum()
     user_null = users_data.isnull().sum()
     product_null = products_data.isnull().sum()
 
+    # Se obtiene que es nulo en la descripcion del producto (solo)
+
     return 0
 
 
 
 def data_shape(train_data, test_data, users_data, products_data):
+
+    # Para ver la cantidad de datos que hay
 
     # vamos con el tamaño de los datasets
     train_shape = train_data.shape
@@ -51,6 +60,8 @@ def data_shape(train_data, test_data, users_data, products_data):
 
 
 def data_description(train_data, test_data, users_data, products_data):
+
+    # MOstramos la descripcion de los databases
 
     # Vemos la descripcion
     user_id_describe = users_data['user_id'].describe()
@@ -107,43 +118,51 @@ def agrupamos(train_data, test_data, users_data, products_data):
     return 0
 
 
-def buscamos_correlacion(train_data, test_data, users_data, products_data):
+def buscamos_correlacion(input_data):
 
-    correlation_train = train_data.corr()
-    print(correlation_train)
+    # Se busca correlacion en el entrenamiento
+    correlation = input_data.corr()
+    print(correlation)
 
     return 0
 
 
-def add_datasets(train_data, test_data, users_data, products_data):
+def add_datasets(train_test_data, users_data, products_data):
+    # En esta funcion Cojemos el train test, y segun el producto y usuario, anadimos la marca del produto, la fecha de union del usuario y el pais
 
     product_list = []
     country_list = []
     user_joined_list = []
 
-    for index, row in train_data.iterrows():
+    for index, row in train_test_data.iterrows():
 
         p_id = row['product_id']
         u_id = row['user_id']
 
+
         row_product = products_data.loc[products_data['product_id'] == p_id]
         row_user = users_data.loc[users_data['user_id'] == u_id]
+        try:
+            product_list.append(row_product['brand_name'].values[0])
+            country_list.append(row_user['country'].values[0])
+            user_joined_list.append(row_user['date_joined'].values[0])
+        except:
+            print('hola')
 
-        product_list.append(row_product['brand_name'])
-        country_list.append(row_user['country'])
-        user_joined_list.append(row_user['date_joined'])
+    train_test_data.loc[:,'product_brand'] = product_list
+    train_test_data.loc[:,'country'] = country_list
+    train_test_data.loc[:,'date_joined'] = user_joined_list
 
-    train_data['product_brand'] = product_list
-    train_data['country'] = country_list
-    train_data['date_joined'] = user_joined_list
-
-    return train_data
+    return train_test_data
 
 if __name__ == '__main__':
     train_data, test_data, users_data, products_data = read_data()
     # buscamos_correlacion(train_data, test_data, users_data, products_data)
 
-    add_datasets(train_data, test_data, users_data, products_data)
+    new_training = add_datasets(train_data,  users_data, products_data)
+    new_testing =  add_datasets(test_data, users_data, products_data)
 
+    new_testing.to_csv('./Exercise/testing_modified.csv', sep=';')
+    new_training.to_csv('./Exercise/training_modified.csv', sep=';')
 
     print('hola')
